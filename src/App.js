62ab6a72b04b3bf43de89728e4e0dcb3fa37c16b
Peter Schussheim <peter@schussheim.com>
2017-01-24 13:20:53 -0500

    refactor(App.js): extract Table and Search components into separate classes

--- src/App.js
+++ src/App.js
@@ -21,26 +21,21 @@ const list = [
 ];
 
 // when a searchTerm is set, match inbound searchTerm pattern with item title
-const isSearched = searchTerm => item => 
-    // condition that returns true or false
-    !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
+const isSearched = searchTerm => item => // condition that returns true or false
+!searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      list,
-      searchTerm: '',
-    };
+    this.state = { list, searchTerm: '' };
 
-    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
 
   onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value })
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
@@ -53,13 +48,30 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text"
-          value={searchTerm}
-          onChange={this.onSearchChange}
-          />
-        </form>
-        { list.filter(isSearched(searchTerm)).map(item =>
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}> {item.title} </a>
@@ -69,7 +81,7 @@ class App extends Component {
             <span> {item.points} </span>
             <span>
               <button
-                onClick={() => this.onDismiss(item.objectID)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss

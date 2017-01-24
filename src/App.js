cb64bdfa627f2daa267ac1042edbbbbd5322be52
Peter Schussheim <peter@schussheim.com>
2017-01-24 15:26:56 -0500

    feat(App.js): add conditional rendering to search feature

--- src/App.js
+++ src/App.js
@@ -58,9 +58,6 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
-    if (!result) { return null }
-
     return (
       <div className="page">
         <div className="interactions">
@@ -68,7 +65,14 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
+        { result
+            ? <Table
+             list={result.hits}
+             pattern={searchTerm}
+             onDismiss={this.onDismiss} 
+            />
+            : null
+        }
       </div>
     );
   }

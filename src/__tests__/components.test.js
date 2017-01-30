62e5311f1999475cc177eb59755cf35dfe3e21f1
Peter Schussheim <peter@schussheim.com>
2017-01-30 11:07:19 -0500

    add unit test for Table

--- src/__tests__/components.test.js
+++ src/__tests__/components.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import App from '../Components/App';
 import Search from '../Components/Search'
 import Button from '../Components/Button'
@@ -59,6 +60,14 @@ describe('Table', () => {
   it('renders', () => {
     const div = document.createElement('div')
     ReactDOM.render(<Table { ...props } />, div)
+  })
+
+  it('shows two items in the list', () => {
+    const element = shallow(
+      <Table { ...props }/>
+    )
+
+    expect(element.find('.table-row').length).toBe(2)
   })
 
   test('snapshots', () => {
